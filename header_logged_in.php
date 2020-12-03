3ee4d3ca3744cd49606e017cde9b798b5bb50047
Rajesh-9679 <rajeshbera201@gmail.com>
2020-12-03 20:03:44 +0530

    First commit

---
+++ header_logged_in.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+</head>
+<body>
+<!--NAVBAR WITH LOGIN -->
+<div class="container-fluid" id="content">
+            <div class="navbar navbar-default navbar-fixed-top" style="background-color: orange;">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>                        
+                        </button>
+                        <a class="navbar-brand" href="index.php"><font color="white"><b>RB Store</b></a></font>
+                    </div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href = "cart.php"><span class = "glyphicon glyphicon-shopping-cart"></span><font color="white"><b> Cart </b></a></font></li>
+                            <li><a href = "settings.php"><span class = "glyphicon glyphicon-setting"></span><font color="white"> <b>Settings </b></a></font></li>
+                            <li><a href = "logout.php"><span class = "glyphicon glyphicon-log-out"></span><font color="white"><b> Logout</b> </a></font></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    </div>
+</body>
+</html>
